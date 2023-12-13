d1de75773c5aa7e361d44cfd68ea9f2508f3fc62
Tiberiu <musattiberiu@gmail.com>
2023-12-13 19:25:34 +0100

    hero section landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,48 @@
-import { twMerge } from "tailwind-merge";
-import { TbDiscountCheckFilled } from "react-icons/tb";
-import { SubLogo } from "../components/SubLogo";
-import { getKey, tests, testsByYear } from "@/tests/tests";
-import { Fragment } from "react";
-import { NavBar } from "../components/NavBar";
-import { fullDate } from "@/tests/fullDate";
+import React from "react";
+import { NAVBAR_HEIGHT, NavBar } from "@/components/NavBar";
+import { Animation } from "@/components/Animation";
 import { Button } from "@/components/Button";
+import { BiMath } from "react-icons/bi";
+import { FaCaretRight, FaDivide } from "react-icons/fa6";
+import { LuTriangle } from "react-icons/lu";
+import { PiCaretDoubleRight } from "react-icons/pi";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Page() {
   return (
-    <main className="bg-math min-h-screen flex flex-col items-center">
-      <NavBar />
-      <h1 className="w-full text-lg font-medium text-left text-black mb-0 min-h-16 justify-center flex flex-col items-center bg-white border-[1px]  border-gray-200 shadow">
-        <div className="w-full max-w-5xl p-4">
-          Teste Evaluarea Națională Matematică
-        </div>
-      </h1>
+    <main className="flex flex-col">
+      <NavBar showStats={false} />
 
-      <div className="flex flex-col gap-4 items-center max-w-4xl w-full px-4">
-        {Object.entries(testsByYear)
-          .sort((a, b) => parseInt(b[0]) - parseInt(a[0]))
-          .map(([byear, subs]) => (
-            <Fragment key={byear}>
-              <h1 className="text-base mt-12 mb-2 font-semibold text-left text-black/40 px-2 py-1 bg-white rounded-md border-[1px] border-gray-200 shadow">
-                {byear}
-              </h1>
-              <div
-                className={twMerge(
-                  "grid w-full max-w-md md:max-w-none md:grid-cols-[1fr_1fr] gap-4 md:[&>*:nth-child(even)]:translate-y-[16px] md:hover:[&>*:nth-child(even)]:translate-y-[18px]",
-                  subs.length == 1 && "md:grid-cols-1 md:max-w-md"
-                )}
-              >
-                {subs.map((s, i) => (
-                  <a key={i} href={`/test/${getKey(s.fullName)}`}>
-                    <Button className="group flex items-center flex-row px-6 py-4 gap-8">
-                      <SubLogo seed={s.id} className="w-8 h-8" />
-                      <div className="flex mr-auto flex-col justify-between">
-                        <div className="text-xl font-normal">{s.name}</div>
-                        <div className="w-fit font-semibold text-sm opacity-40">
-                          {fullDate(s.date)}
-                        </div>
-                      </div>
-                      {null && (
-                        <TbDiscountCheckFilled className="text-green-500 text-4xl" />
-                      )}
-                    </Button>
-                  </a>
-                ))}
-              </div>
-            </Fragment>
-          ))}
+      <div
+        className="flex flex-col sm:flex-row items-center justify-center gap-4 lg:gap-16 p-4 w-full bg-math"
+        style={{
+          minHeight: `calc(100vh - ${NAVBAR_HEIGHT})`,
+          paddingBottom: `calc(1rem + ${NAVBAR_HEIGHT})`,
+        }}
+      >
+        <div className="w-64 sm:w-96 aspect-square">
+          <Animation src="/animations/fig1.riv" stateMachines="main" />
+        </div>
+        <div className="flex flex-col max-w-xl items-center sm:items-start">
+          <h1 className="text-3xl lg:text-5xl font-bold text-black bg-anti-math text-center sm:text-left balance-lines">
+            Teste interactive cu
+            <br />
+            rezolvări video
+          </h1>
+          <h2 className="text-xl lg:text-3xl font-normal text-black bg-anti-math mt-8 text-center sm:text-left balance-lines">
+            Prima platformă educațională gratuită dedicată Evaluării Naționale
+          </h2>
+          <Link href="/teste-matematica">
+            <Button className="px-6 py-2 w-fit font-medium text-lg mt-8 flex gap-4 items-center sm:-ml-1">
+              {/* <BiMath /> */}
+              Teste matematică
+              <FaAngleDoubleRight />
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 items-center">
         <div className="mb-4 mt-28 font-medium text-black/40 bg-anti-math flex items-center gap-1">
           <span className="text-2xl">©</span> 2023 ZeceLaEN.ro
         </div>
