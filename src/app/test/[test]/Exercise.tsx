cc96bc7e6e432b53731c2351f7238e50423a3c5b
Tiberiu <musattiberiu@gmail.com>
2023-12-14 22:25:00 +0100

    mobile full width video

--- src/app/test/[test]/Exercise.tsx
+++ src/app/test/[test]/Exercise.tsx
@@ -204,7 +204,7 @@ export function Exercise({
         <div className="flex flex-col gap-4 mt-8">
           <div className="font-semibold text-lg">Rezolvare</div>
 
-          <div className="relative aspect-video w-full rounded-2xl overflow-hidden shadow bg-black">
+          <div className="relative w-screen self-center sm:w-full shadow overflow-hidden sm:rounded-2xl bg-black">
             <svg
               className="spinner absolute top-1/2 left-1/2 transform -mx-6 -my-6 w-12 h-12"
               width="65px"
@@ -223,11 +223,12 @@ export function Exercise({
             </svg>
             <YouTube
               videoId={videoUrl?.split("=").at(-1) || ""}
-              className="h-full w-full absolute"
-              iframeClassName="h-full w-full"
+              className="relative w-full aspect-video" // -my-[3%]"
+              iframeClassName="h-full w-full" //h-[1000%] w-[1000%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[0.1]"
               opts={{
                 playerVars: { rel: 0 },
               }}
+              title="YouTube video player"
             />
           </div>
         </div>
